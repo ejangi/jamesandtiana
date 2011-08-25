24d5b3f5833b02acad1e6a6d9a6deb8908292501
ejangi <ejangidotcom@gmail.com>
2011-08-25 20:16:43 +1000

    Fixed ordering of RSVPS

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,6 +2,8 @@ class Rsvp < ActiveRecord::Base
   belongs_to :user
   belongs_to :registry
   
+  default_scope order("registries.id ASC, rsvps.attending DESC, rsvps.number_of_guests DESC")
+  
   scope :by_attending, lambda {
     includes(:registry).order("registries.id, rsvps.attending DESC")
   }
