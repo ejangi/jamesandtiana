0e3a7e00583a8974faf48825a9baf6c1a7717d6a
ejangi <ejangidotcom@gmail.com>
2011-08-27 11:04:26 +1000

    Fixed bug when displaying RSVPs

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,7 +2,7 @@ class Rsvp < ActiveRecord::Base
   belongs_to :user
   belongs_to :registry
   
-  default_scope order("registries.id ASC, rsvps.attending DESC, rsvps.number_of_guests DESC")
+  default_scope order("rsvps.registry_id ASC, rsvps.attending DESC, rsvps.number_of_guests DESC")
   
   scope :by_attending, lambda {
     includes(:registry).order("registries.id, rsvps.attending DESC")
