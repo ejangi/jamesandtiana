553fd00767c73b29d91a7ce0c86ca8ccad4b4517
ejangi <ejangidotcom@gmail.com>
2011-08-28 22:36:32 +1000

    Attempt to get exception notifier working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,9 @@ Jamesandtiana::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.smtp_settings = {
+  #     :enable_starttls_auto => false
+  #   }
 
   # Enable threaded mode
   # config.threadsafe!
